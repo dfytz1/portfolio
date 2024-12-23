e87db65992bdce63cd3b32f35a4c96704549e598
dfytz1 <dfytz1@gmail.com>
2024-12-23 10:51:34 +0200

    Centered page contents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-white`}>
         <main className="flex flex-col md:flex-row min-h-screen">
           <Navigation className="w-full md:w-[300px] md:min-h-screen" />
-          <div className="flex-1 w-full">
+          <div className="flex-1 w-full flex justify-center">
             {children}
           </div>
         </main>
